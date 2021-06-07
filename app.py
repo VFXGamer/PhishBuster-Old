38c0d557d76f732f9a174df48a611409d77b2c46
VFXGamer <62838631+VFXGamer@users.noreply.github.com>
2021-06-07 08:35:08 +0530

    Fixed API Output

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template, redirect
+from flask import Flask, request, render_template, redirect, jsonify
 import phishbuster as pb
 
 app = Flask(__name__,template_folder='static')
