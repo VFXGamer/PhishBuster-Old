d307c9b1227f83251d2902752407b3ecb7f148c1
VFXGamer <62838631+VFXGamer@users.noreply.github.com>
2021-07-21 10:37:37 +0530

    Improved phishing detection and reports

--- app.py
+++ app.py
@@ -1,102 +1,129 @@
-from flask import Flask, request, render_template, redirect, jsonify
+from flask import Flask, request, render_template, redirect, jsonify, flash, url_for
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
 import phishbuster as pb
 from flaskext.mysql import MySQL
+import requests
 import os
 
 app = Flask(__name__,template_folder='static')
-limiter = Limiter(app, key_func=get_remote_address)
+limiter = Limiter(app, key_func=get_remote_address) # Limiter setup for PhishBuster API
 mysql = MySQL()
 
 app.config['MYSQL_DATABASE_USER'] = os.environ['user']
 app.config['MYSQL_DATABASE_PASSWORD'] = os.environ['password']
 app.config['MYSQL_DATABASE_DB'] = os.environ['dbname']
 app.config['MYSQL_DATABASE_HOST'] = os.environ['servername']
+mysql.init_app(app) 
 
-mysql.init_app(app)
+header = ['Sr No.', 'Orginal Site', 'Phishing Site','Action'] # Header for reports table
 
-header = ['Sr No.', 'Orginal Site', 'Phishing Site']
+connect = mysql.connect() # for connecting to the database
+cursor = connect.cursor() # cursor to execute mysql queries
 
-connect = mysql.connect()
-cursor = connect.cursor()
-
-def mysqldata(seurl,inurl):
+def mysqldata_insert(seurl,inurl): # For appending values to reports_data table
     try:
-        cursor.execute(f"INSERT INTO reports_data(org_site,phish_site) VALUES ('{seurl}','{inurl}')")
-        connect.commit()
+        cursor.execute(f"INSERT INTO reports_data(org_site,phish_site) VALUES ('{seurl}','{inurl}')") # mysql query to append data to the dataabase
+        connect.commit() # commit changes to database
         print('Commited Successfully')
     except Exception as e:
         print(e)
-        connect.rollback()
+        connect.rollback() # undo changes if error occured while appending data to the database
 
+# index page
 @app.route("/")
 def index():
     try:
-        #execute select statement to fetch data to be displayed in dropdown
-        cursor.execute('SELECT names,domains FROM domain_data')
-        db_output = cursor.fetchall()
-        lis = list(db_output)
-        lis.sort()
-        selecturl = [["Select","select"]]+lis
-        return render_template("index.html",selecturl=selecturl)
+        cursor.execute('SELECT names,domains FROM domain_data') # mysql query to get all the data from the database
+        db_output = cursor.fetchall() # fetching all the data from the database
+        lis = list(db_output) # converting tuple to list
+        lis.sort() # sorting the list
+        selecturl = [["Select Site","select"]]+lis # appending the list to the list
+        cursor.execute('SELECT country_name,country_code FROM countries') # mysql query to get all the data from the database
+        db_output2 = cursor.fetchall() # fetching all the data from the database
+        lis2 = list(db_output2) # converting tuple to list
+        lis2.sort() # sorting the list
+        countrydata = [["Select Country","select"]]+lis2 # appending the list to the list
+        return render_template("index.html",selecturl=selecturl,countrydata=countrydata) # selecturl sends list containing list of real sites
     except Exception as e:
         print(e)
-        selecturl = [["Error Occured to connect with DB","select"]]
-        return render_template("index.html",selecturl=selecturl)
+        selecturl = [["Error Occured to connect with DB","select"]] # if error occured while fetching data from the database
+        countrydata = [["Error Occured to connect with DB","select"]] # if error occured while fetching data from the database
+        return render_template("index.html",selecturl=selecturl,countrydata=countrydata) # selecturl sends error as it failed to get data from the database
     return redirect('/')
 
+# For getting values from index and passing them to PhishBuster
 @app.route('/check', methods=["POST"])
 def check():
     if request.method == "POST":
-        req = request.form
+        req = request.form 
         inurl = req['inurl'] # Storing input url in a variable
         seurl = req['seurl'] # Storing url from drop down menu in a variable
-        if inurl != '' and seurl != 'select':
-            output = pb.comparing_url(inurl,seurl)
+        country = req['country'] # Storing country's iso code in a variable
+        inurl = inurl.lower() # Converting input url to lower case to avoid errors
+        if inurl != '' and seurl != 'select' and country != 'select':
+            output = pb.comparing_url(inurl,seurl,country) # Returns 'True' if it is a phishing site or 'False' if it is a safe site
             if output is True:
-                mysqldata(seurl,inurl)
+                mysqldata_insert(seurl,inurl) # For appending data if it is a phising site
                 return redirect('/phishing') # Redirects to It is  PHISHING SITE
-            else:
-                return redirect('/safe') # Redirects to It is SAFE SITE
-        else:
-            return redirect('/') # Redirects to home page if vlaues are not entered
-    return redirect('/')
+            return redirect('/safe') # Redirects to It is SAFE SITE
+        return redirect('/') # Redirects to home page if values are not entered
 
+# reports page
 @app.route("/reports")
 def reports():
     try:
-        #execute select statement to fetch data to be displayed in dropdown
-        cursor.execute('SELECT * FROM reports_data')
+        cursor.execute('SELECT * FROM reports_data') # '*' here is for id, orginal site and phising site
         db_output = cursor.fetchall()
-        lis = list(db_output)
-        return render_template("reports.html",head=header,reports = lis)
+        # converting to list to pass the data to html
+        reports = list(db_output)
+        return render_template("reports.html",head=header,reports=reports) # header for column names and reports for rows/site data
     except Exception as e:
         print(e)
-        lis = ["Error Occured to connect with DB"]
-        return render_template("reports.html",head=header,reports = lis)
+        reports = ["Error Occured to connect with DB"] # if error occured while fetching data from the database
+        return render_template("reports.html",head=header,reports=reports)
     return redirect('/')
 
+# phising label page
 @app.route("/phishing")
 def phish():
     return render_template("phish.html")
 
+# safe label page
 @app.route("/safe")
 def safe():
     return render_template("safe.html")
 
-@app.route("/api/<string:inurl>+<string:seurl>+<string:save>")
-@limiter.limit("50/minute")
-def api(inurl,seurl,save = 'False'):
-    inurl.lower()
-    seurl.lower()
-    output = pb.comparing_url(inurl,seurl)
-    if save == 'True':
+# For deleteing a desired row   
+@app.route('/delete/<id>/')
+def delete(id):
+    cursor.execute(f"DELETE FROM reports_data WHERE id='{id}';") # mysql query to delete data of a row
+    connect.commit() # commit changes to the database
+    print('Commited Successfully')
+    return redirect('/reports') # Redirecting to reports page to show that changes were made successfully
+
+# For manually adding values to the database of reports
+@app.route('/manualadd', methods = ['POST'])
+def manualadd():
+    if request.method == 'POST':
+        req = request.form
+        seurl = req['org'] # getting values through post request and storing to a variable
+        inurl = req['phish'] # getting values through post request and storing to a variable
+        mysqldata_insert(seurl,inurl) # To adding data to the database
+        return redirect('/reports') # Redirecting to reports page to show that changes were made successfully
+
+@app.route("/api/<string:inurl>+<string:seurl>+<string:country>+<string:save>") # Geeting values from the url
+@limiter.limit("50/minute") # Setting a limit of 50 PhishBuster API requests in a minute
+def api(inurl,seurl,country,save = 'False'):
+    inurl = inurl.lower() # Converting input url to lower case to avoid errors
+    seurl = seurl.lower() # Converting url from original domain to lower case to avoid errors
+    output = pb.comparing_url(inurl,seurl,country) # Returns 'True' if it is a phishing site or 'False' if it is a safe site
+    if save == 'True': # String as the input can be made to anything and to overcoming error if the input is coverted to boolean before filtering
         if output is True:
-            mysqldata(seurl,inurl)
-            return jsonify({'Input Url':inurl,'Orginal Url':seurl,'Phishing Site':output,'Data Saved':bool(save)})
-        return jsonify({'Input Url':inurl,'Orginal Url':seurl,'Phishing Site':output,}) 
-    return jsonify({'Input Url':inurl,'Orginal Url':seurl,'Phishing Site':output})
+            mysqldata_insert(seurl,inurl) # To adding data to the database
+            return jsonify({'Input Url':inurl,'Orginal Url':seurl,'Phishing Site':output,'Data Saved':bool(save),'Region':country}) # API response with data saved information
+        return jsonify({'Input Url':inurl,'Orginal Url':seurl,'Phishing Site':output,'Region':country}) # API response if values are not saved
+    return jsonify({'Input Url':inurl,'Orginal Url':seurl,'Phishing Site':output,'Region':country}) # API response if values are not saved
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
--- call_api.py
+++ call_api.py
@@ -3,24 +3,28 @@ import requests
 import re
 
 def url_syntax(url_changes):
-    url_search_http = re.search("http", url_changes)
+    url_search_http = re.search("http", url_changes) # for finding http using regular expressions
     if url_search_http is None:
-        url_http = "http://" + url_changes
+        url_http = "http://" + url_changes # adding http:// at the start if it is not present
     else:
-        url_http = url_changes
-    return url_http # Returns the url with 'http://' if not there in the input url
-
-def api_call(inurl, seurl):
+        url_http = url_changes # if http:// is present the return the input url
+    return url_http # Returns the url with 'http://'
+def api_call(inurl, seurl,country, store = 'False'):
     '''
     "inurl" is the input url which is suspected to be phishing site.
     "seurl" is the select url in which only the domain name of the orginal site is to be given as in put.
+    "country" here is the iso code of the country that is 'in' for 'India' etc.
     '''
-    check = url_syntax (inurl)
-    inurl = urlparse(check).netloc
-    URL = "https://phishbuster-web.herokuapp.com/api/"+ inurl + '+' + seurl
-    req = requests.get(url = URL)
-    return req.json()
+    iinurl = inurl.lower()
+    seurl = seurl.lower()
+    country = country.lower()
+    check = url_syntax (inurl) # adds http:// if not there in the url
+    inurl = urlparse(check).netloc # removing path and https:// from a url
+    # inurl is the input url, seurl is the orginal domain and store is the permission to store data if it is a phishing site
+    URL = "http://phishbuster-web.herokuapp.com/api/"+ inurl + '+' + seurl + '+' + country + '+' + store
+    req = requests.get(url = URL) # sends a get request to the PhishBuster API
+    return req.json() # Reading data with json
 
 if __name__ == '__main__':
-    print(api_call('https://www.microsoft.com~@www.google.com/wsgrye/ruygfbryu/gijgnuf','google.com'))
+    print(api_call(inurl='https://www.microsoft.com~@www.google.com/wsgrye/ruygfbryu/gijgnuf',seurl='google.com',country='in',store='False'))
 
